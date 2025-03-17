6095fb4d1f54ffab2d9178f4be382851a0a8b9ae
Anshuman Rath <anshumanrath030@gmail.com>
2025-03-17 22:47:10 +0530

    .....sdevrb

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 
 from .views import user_login,user_logout
 urlpatterns = [
-    path("accounts/login/",  user_login, name="admin_login"),
-    path("accounts/logout/", user_logout, name="admin_logout"),
+    path("login/",  user_login, name="admin_login"),
+    path("logout/", user_logout, name="admin_logout"),
 ]
