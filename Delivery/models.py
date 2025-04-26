837d51706f43e373260f3050b3c9eb0d86842ebe
Anshuman Rath <anshumanrath030@gmail.com>
2025-04-26 22:44:51 +0530

    ..

--- Delivery/models.py
+++ Delivery/models.py
@@ -11,3 +11,6 @@ class ShiprocketOrder(models.Model):
     awb_code = models.CharField(max_length=50)
     courier_name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(f"{self.id} - order {self.order}  - shiprocket order - {self.shiprocket_order_id}")
