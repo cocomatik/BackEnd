23ba588afbf30b6b2d95458365ba1547e89c86c7
Anshuman Rath <anshumanrath030@gmail.com>
2025-04-28 21:25:49 +0530

    ....

--- Orders/bills.py
+++ Orders/bills.py
@@ -5,16 +5,16 @@ from rest_framework.response import Response
 from rest_framework import status
 from xhtml2pdf import pisa
 from .models import Order
+import io
 
 @api_view(['GET'])
 def generate_invoice(request, order_number):
     try:
-        # Fetch order data from the database
         order = Order.objects.get(order_number=order_number)
     except Order.DoesNotExist:
         return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
 
-    # Prepare the context for the template
+    # Prepare context for the HTML template
     context = {
         'order': order,
         'order_items': order.cart.cart_items.all(),
@@ -22,17 +22,7 @@ def generate_invoice(request, order_number):
         'address': order.address,
     }
 
-    # Render HTML from template
+    # Render HTML template with context data
     html_content = render_to_string('Orders/Bills/invoice_template.html', context)
 
-    # Create an HTTP response object with the content type for PDF
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'attachment; filename="invoice_{order.order_number}.pdf"'
-
-    # Use xhtml2pdf to convert HTML to PDF
-    pisa_status = pisa.CreatePDF(html_content, dest=response)
-
-    if pisa_status.err:
-        return Response({"error": "Error generating PDF"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    return response
+    return HttpResponse(html_content)
