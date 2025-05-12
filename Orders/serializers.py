55ba4c5df3b7bb26d0f3157a7f1e17ad1bd5f8b9
Anshuman Rath <anshumanrath030@gmail.com>
2025-05-12 22:09:34 +0530

    ..

--- Orders/serializers.py
+++ Orders/serializers.py
@@ -66,7 +66,6 @@ class CartSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     address = AddressSerializer()
-    user = UserSerializer()
 
     class Meta:
         model = Order
